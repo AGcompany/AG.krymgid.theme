02886f06abab1d02de387d74dbfdd20d70c0c53c
Bogdan Girman <bogdan.girman@gmail.com>
2012-08-07 22:02:56 +0300

    Rename README.rst

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0.0'
 setup(name='AG.krymgid.theme',
       version=version,
       description="AG.krymgid.theme package",
-      long_description=open("README.txt").read() + "\n" +
+      long_description=open("README.rst").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
